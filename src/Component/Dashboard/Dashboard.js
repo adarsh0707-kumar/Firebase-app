1bf404af8212c3df297713be90965680b4030fa6
Adarsh Kumar <adarshku123456789@gmail.com>
2024-10-30 17:57:20 +0530

    LogOut

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -2,8 +2,9 @@ import React from 'react';
 import '../Style/Dashboard__Style.css';
 import SideNavBar from './SideNavBar';
 import MainContent from './MainContent';
-
 const Dashboard = () => {
+
+
     return (
         <div className='dashboard'>
             <SideNavBar />
@@ -11,5 +12,6 @@ const Dashboard = () => {
         </div>
     )
 }
+
 
 export default Dashboard
--- src/Component/Dashboard/SideNavBar.js
+++ src/Component/Dashboard/SideNavBar.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Style/Dashboard__Style.css'
+import '../Style/Dashboard__Style.css';
+import { app } from '../../Firebase';
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
 
 const SideNavBar = () => {
+
+    const navigate = useNavigate();
+    const LogOut = () => {
+        const auth = getAuth(app);
+        signOut(auth)
+            .than(res => {
+                navigate('/login');
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
     return (
         <div className='dashboard__sideNavbar'>
             <Link className='dashboard__sideNavbar__link' to='/dashboard/student'>Student </Link>
             <Link className='dashboard__sideNavbar__link' to='/dashboard/faculty'>Faculty </Link>
             <Link className='dashboard__sideNavbar__link' to='/signup'>SignUp</Link>
-            <Link className='dashboard__sideNavbar__link' to='/login'>LogOut </Link>
+            <button className='dashboard__sideNavbar__link' onClick={LogOut}>LogOut </button>
         </div>
     )
 }
