a7b68f5d47721d0e837e39c00c87a2078fcc55b5
Adarsh Kumar <adarshku123456789@gmail.com>
2024-10-30 18:11:13 +0530

    login

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../Style/Dashboard__Style.css';
 import SideNavBar from './SideNavBar';
 import MainContent from './MainContent';
+import { app } from '../../Firebase';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 const Dashboard = () => {
-
+    useEffect(() => {
+        const auth = getAuth(app);
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log('yes login', user);
+            }
+            else {
+                console.log('not login');
+            }
+        })
+        return () => unsubscribe();
+    },[])
 
     return (
         <div className='dashboard'>
