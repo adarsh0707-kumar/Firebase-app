00abb6f9f5e7ed26970194ea4cd3b527e6b4375c
Adarsh Kumar <adarshku123456789@gmail.com>
2024-10-30 11:48:11 +0530

    send

--- src/Component/Authentication/Login.js
+++ src/Component/Authentication/Login.js
@@ -106,7 +106,8 @@ const Login = () => {
                 <br />
                 <br />
                 <h2>Login With OTP</h2>
-                <input onChange={(e)=>{setPhone(e.target.value)}} placeholder='Phone Number' type='number' />
+                <input onChange={(e) => { setPhone(e.target.value) }} placeholder='Phone Number' type='number' />
+                <div id = 'abc'></div>
                 <button type='button' onClick={sendOTP}>Send OTP</button> 
             </form>
 
